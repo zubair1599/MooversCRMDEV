ae2eb28729f285b24cfa347edd51bd6ea25e9b2d
zubair <zubair1599@hotmail.com>
2013-11-13 23:11:27 +0400

    table change

--- Tables/dbo.temptable.sql
+++ Tables/dbo.temptable.sql
@@ -1,7 +1,8 @@
 CREATE TABLE [dbo].[temptable]
 (
 [testcol] [int] NOT NULL,
-[Name] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[Name] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Year] [int] NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[temptable] ADD CONSTRAINT [PK_temptable] PRIMARY KEY CLUSTERED  ([testcol]) ON [PRIMARY]
